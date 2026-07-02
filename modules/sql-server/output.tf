5da3a99ac07ffb212d5e85560ff0198890c1837f
Lavanya <lavanyaanbu@gmail.com>
2026-07-02 18:06:25 +0530

    tf fix

---
+++ modules/sql-server/output.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = azurerm_mssql_server.this.id
+}
+
+output "name" {
+  value = azurerm_mssql_server.this.name
+}
+
+output "fqdn" {
+  value = azurerm_mssql_server.this.fully_qualified_domain_name
+}
